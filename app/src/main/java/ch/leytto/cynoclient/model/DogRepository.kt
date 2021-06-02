31dc7db50728f6187f0200f94cf8cbd85462c13a
Hausmann <william.hausmann@cpnv.ch>
2021-06-02 14:47:07 +0200

    Add floating button & Icon app

--- app/src/main/java/ch/leytto/cynoclient/model/DogRepository.kt
+++ app/src/main/java/ch/leytto/cynoclient/model/DogRepository.kt
@@ -9,7 +9,7 @@ class DogRepository(private val dogDao: DogDao) {
 
     // Room executes all queries on a separate thread.
     // Observed Flow will notify the observer when the data has changed.
-    val allWords: List<Dog> = dogDao.getDogs()
+    val allDogs: List<Dog> = dogDao.getDogs()
 
     // By default Room runs suspend queries off the main thread, therefore, we don't need to
     // implement anything else to ensure we're not doing long running database work
